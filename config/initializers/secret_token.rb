bcc331408b5ea0398551c545f5e4f1cb3c016462
Tim Schurig <T.schurig@gmail.com>
2013-11-27 13:42:53 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasklove::Application.config.secret_token = '5d982796c08b2b3652bd5e882f2583ce9ddf37986fd580405292f6516fce0fb6f72a025481f7a03dd94bf08054c120f708f2cf688a2e2180d89736bc003118f1'
